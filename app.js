7447022913db672dc944f30851eba6d14e4feacd
Gaurav Chaurasia <gaurav.181cv155@nitk.edu.in>
2020-09-29 12:02:49 +0530

    fix callback hell with .then().catch()

--- app.js
+++ app.js
@@ -6,36 +6,39 @@ const dboper = require('./operation');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'complete_backend';
 
-MongoClient.connect(url, (err, client) => {
-    
-    assert.equal(err, null);
+MongoClient.connect(url).then((client) => {
 
-    console.log(`Connected correctly to server`);
-
+    console.log('Connected correctly to server');
     const db = client.db(dbname);
 
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},"dishes")
+        .then((result) => {
             console.log("Insert Document:\n", result.ops);
+            
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+            return dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, "dishes");
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
                             
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
 
-                                client.close();
-                            });
-                        });
-                    });
-            });
-    });
-});
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+
+})
+.catch((err) => console.log(err));
